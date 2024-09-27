6ebd8f78670b1979fd51180cee2c5f3e199c3ed3
VishalKotak <kotakvishal2001@gmail.com>
2024-09-27 14:10:40 +0530

    first commit

---
+++ app/src/main/java/ktk/wishdroid/dependencyinjectiondemo/DependecyInjectionDemoApp.kt
@@ -0,0 +1,8 @@
+package ktk.wishdroid.dependencyinjectiondemo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DependecyInjectionDemoApp:Application(){
+}
